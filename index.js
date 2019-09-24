6f0b625b8628cabebfc36629feb90cc9181b36cb
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2019-09-24 20:02:35 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,11 @@
+const Scooter = (year, color, model) => {
+  this.year = year;
+  this.color = color;
+  this.model = model;
+};
+
+const Driver = (name, age, experience) => {
+  this.name = name;
+  this.age = age;
+  this.experience = experience;
+}
