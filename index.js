710f27bc8ce9e4c98a4ac4b1a46058a023270058
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2019-09-24 20:04:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-const Scooter = (year, color, model) => {
+function Scooter(year, color, model) {
   this.year = year;
   this.color = color;
   this.model = model;
 };
 
-const Driver = (name, age, experience) => {
+function Driver(name, age, experience) {
   this.name = name;
   this.age = age;
   this.experience = experience;
 };
 
-const PickupLocation = (address, city) => {
+function PickupLocation(address, city) {
   this.address = address;
   this.city = city;
 };
